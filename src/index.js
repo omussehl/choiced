fb581dcec8335b9838a4634a373bf5c2f66980ab
Omar Mussehl <omar@Omars-MacBook-Pro.local>
2022-09-13 21:30:08 -0400

    lets try this again

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ root.render(
   </React.StrictMode>
 );
 
-//adding a comment to test something
+//adding a comment to test something // test 2
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
