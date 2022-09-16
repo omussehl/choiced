47f0b6f4648087aa5c0259836829f698db74251d
Omar Mussehl <omar@Omars-MacBook-Pro.local>
2022-09-16 00:27:01 -0400

    replaced most of code, and am using a lot of this as a template for my project, so will change a lot of what you currently see but use the skeleton

---
+++ mern/server/db/conn.js
@@ -0,0 +1,27 @@
+// this connexts our code to the database - config.env is the driver code that connects the mongodb cluster
+const { MongoClient } = require("mongodb");
+const Db = process.env.ATLAS_URI;
+const client = new MongoClient(Db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+ 
+var _db;
+ 
+module.exports = {
+  connectToServer: function (callback) {
+    client.connect(function (err, db) {
+      // Verify we got a good "db" object
+      if (db)
+      {
+        _db = db.db("employees");
+        console.log("Successfully connected to MongoDB."); 
+      }
+      return callback(err);
+         });
+  },
+ 
+  getDb: function () {
+    return _db;
+  },
+};
